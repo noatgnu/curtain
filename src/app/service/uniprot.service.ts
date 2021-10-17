ffa992fe96ffcc3bfaa48b4a14e493623d3386df
Toan Phung <toan.phungkhoiquoctoan@gmail.com>
2021-10-17 22:32:27 +0100

    update

--- src/app/service/uniprot.service.ts
+++ src/app/service/uniprot.service.ts
@@ -12,7 +12,6 @@ import {stringify} from "@angular/compiler/src/util";
 export class UniprotService {
   private baseURL = 'https://www.uniprot.org/uploadlists/?';
   public Re = /[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}/;
-
   private base = 'https://www.uniprot.org';
   private toolEndpoint = '/uploadlists/?';
 
