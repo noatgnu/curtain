957f5d5fb692730abc0464c32e1dc2c1eb0053d5
Toan Phung <toan.phungkhoiquoctoan@gmail.com>
2022-08-29 10:46:11 +0100

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiURL: "http://www.conducto.me/"
+  apiURL: "http://curtain.omics.quest/"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiURL: "http://www.conducto.me/"
+  apiURL: "http://curtain.omics.quest/"
 };
 
 /*
