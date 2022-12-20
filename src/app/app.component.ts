b17b90017ee5abb6a42a899f4a9079d10b2e945b
Toan Phung <toan.phungkhoiquoctoan@gmail.com>
2022-12-20 15:28:59 +0000

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,12 @@ export class AppComponent {
   constructor() {
     if (document.URL.includes("#access_token" )) {
       const l = document.URL.replace(window.location.origin+"/#", "")
-      console.log(l)
+      const data: any[] = []
+      for (const i of l.split("&")) {
+        for (const d of i.split("=")) {
+          data.push(d)
+        }
+      }
     }
 
   }
