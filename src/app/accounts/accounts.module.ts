18b80334f2e1360820acb1b9faa27101fcad110a
Toan Phung <toan.phungkhoiquoctoan@gmail.com>
2023-01-24 12:50:34 +0000

    update

--- src/app/accounts/accounts.module.ts
+++ src/app/accounts/accounts.module.ts
@@ -1,10 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SocialLoginModule, SocialAuthServiceConfig } from '@abacritt/angularx-social-login';
-import {
-  GoogleLoginProvider,
-  FacebookLoginProvider
-} from '@abacritt/angularx-social-login';
 import {AccountsComponent} from "./accounts/accounts.component";
 import {LoginModalComponent} from "./login-modal/login-modal.component";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
@@ -20,35 +15,13 @@ import {NgbDropdownModule, NgbNavModule, NgbPaginationModule} from "@ng-bootstra
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    SocialLoginModule,
     NgbPaginationModule,
     NgbNavModule,
     FormsModule,
     NgbDropdownModule,
-
   ],
   providers: [
-    {
-      provide: 'SocialAuthServiceConfig',
-      useValue: {
-        autoLogin: false,
-        providers: [
-          /*{
-            id: GoogleLoginProvider.PROVIDER_ID,
-            provider: new GoogleLoginProvider(
-              environment.google
-            )
-          },
-          {
-            id: FacebookLoginProvider.PROVIDER_ID,
-            provider: new FacebookLoginProvider('clientId')
-          }*/
-        ],
-        onError: (err) => {
-          console.error(err);
-        }
-      } as SocialAuthServiceConfig,
-    }
+
   ]
 })
 export class AccountsModule { }
