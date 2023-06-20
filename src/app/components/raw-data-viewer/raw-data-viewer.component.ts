59caab96d9a28eb858474c06a695f126ff8149b3
Toan Phung <toan.phung@proteo.info>
2023-06-20 10:43:54 +0100

    update

--- src/app/components/raw-data-viewer/raw-data-viewer.component.ts
+++ src/app/components/raw-data-viewer/raw-data-viewer.component.ts
@@ -2,6 +2,8 @@ import {Component, Input, OnInit} from '@angular/core';
 import {DataFrame, IDataFrame} from "data-forge";
 import {DataService} from "../../data.service";
 import {UniprotService} from "../../uniprot.service";
+import {FormBuilder} from "@angular/forms";
+import {debounceTime, distinctUntilChanged, map} from "rxjs";
 
 @Component({
   selector: 'app-raw-data-viewer',
@@ -13,8 +15,48 @@ export class RawDataViewerComponent implements OnInit {
 
   @Input() set data(value: IDataFrame) {
     this._data = value
+    this.displayDF = value
   }
-  constructor(public dataService: DataService) { }
+
+  form = this.fb.group({
+    filterTerm: [""],
+    filterType: ["Gene Names"],
+  })
+  displayDF: IDataFrame = new DataFrame()
+  constructor(public dataService: DataService, private fb: FormBuilder) {
+    this.form.controls["filterTerm"].valueChanges.pipe(debounceTime(200), distinctUntilChanged()).subscribe((value) => {
+      let primaryIds: string[] = []
+      if (value){
+        if (value.length > 2) {
+          switch (this.form.controls["filterType"].value) {
+            case "Gene Names":
+              const genes = this.dataService.selectedGenes.filter((gene: string) => gene.toLowerCase().includes(value.toLowerCase()))
+              genes.forEach((gene: string) => {
+                  primaryIds.push(...this.dataService.getPrimaryIDsFromGeneNames(gene))
+              })
+              break
+            case "Primary IDs":
+              primaryIds = this.dataService.selected.filter((primaryID: string) => primaryID.toLowerCase().includes(value.toLowerCase()))
+              break
+          }
+          if (value === "") {
+            this.displayDF = this._data
+          } else if (primaryIds.length > 0) {
+            this.displayDF = this._data.where((row: any) => primaryIds.includes(row[this.dataService.rawForm.primaryIDs]))
+          } else {
+            this.displayDF = new DataFrame()
+          }
+        }
+
+        console.log(this.displayDF)
+        console.log(primaryIds)
+
+      } else {
+        this.displayDF = this._data
+      }
+
+    })
+  }
 
   ngOnInit(): void {
   }
