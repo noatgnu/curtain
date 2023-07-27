dfcd7ea2a14aaa12ed2df41606746f51446a8de9
Toan Phung <toan.phungkhoiquoctoan@gmail.com>
2023-07-27 12:06:19 +0100

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  //apiURL: "https://celsus.muttsu.xyz/",
-  apiURL: "https://celsus.caprover.muttsu.com/",
+  apiURL: "https://celsus.muttsu.xyz/",
+  //apiURL: "https://celsus.caprover.muttsu.com/",
   //google: "101803197167-s9fb349oeqhjmvs5afn9idopgipnlvtk.apps.googleusercontent.com",
   orcid: "APP-5RXLC3W1MS2MOW0F"
 };
