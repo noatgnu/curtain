3de62dacee7170accde544c28ef82b324e485014
Toan Phung <toan.phung@proteo.info>
2023-09-29 09:40:22 +0100

    update

--- src/app/components/qrcode-modal/qrcode-modal.component.ts
+++ src/app/components/qrcode-modal/qrcode-modal.component.ts
@@ -17,7 +17,7 @@ export class QrcodeModalComponent implements OnInit {
     width: 250,
     height: 250,
     data: this.url,
-    image: "assets/favicon.128x128.png",
+    //image: "assets/favicon.128x128.png",
     margin: 5,
     dotsOptions: {
       color: "#2E2D62",
