e6aacacc956c232bcb9bfb922fa5710e18267f39
Toan Phung <toan.phung@proteo.info>
2024-01-10 19:23:36 +0000

    update

--- src/app/components/encryption-settings/encryption-settings.component.ts
+++ src/app/components/encryption-settings/encryption-settings.component.ts
@@ -107,5 +107,6 @@ export class EncryptionSettingsComponent {
     // remove keys from local storage
     localStorage.removeItem("public_key")
     localStorage.removeItem("private_key")
+    this.toastService.show("Encryption", "Keys cleared").then()
   }
 }
